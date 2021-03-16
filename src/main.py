7e5b9a5ac2fc59c5fd9018fd3fc3ca4d8f512d39
Ville Kopio <ville.kopio@kodan.fi>
2021-03-16 14:49:32 +0200

    Small formatting fix.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import torch
 
 def main():
     tensor = torch.tensor([[1., -1.], [1., -1.]])
-    print('Hello,\n', tensor, '!')
+    print('Hello,\n{}!'.format(tensor))
 
 
 if __name__ == '__main__':
