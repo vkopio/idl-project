c2ad862738f870bd827347466478f5df651dbd2b
Ville Kopio <ville.kopio@kodan.fi>
2021-03-20 19:44:53 +0200

    Add initial data loading to main.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
 import torch
+import torch.utils.data as data
+from torchvision import transforms
+
+from data_set import ImageDataSet
+
+BATCH_SIZE = 100
 
 
 def main():
-    tensor = torch.tensor([[1., -1.], [1., -1.]])
-    print('Hello,\n{}!'.format(tensor))
+    transformers = [
+        transforms.ToTensor(),
+    ]
+
+    data_set = ImageDataSet('data/images', transforms.Compose(transformers))
+
+    train_loader = data.DataLoader(
+        data_set,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        num_workers=4,
+    )
 
 
 if __name__ == '__main__':
