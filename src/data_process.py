9fd4534cd3f60fa652d15aeaa26b2cbf5d531179
Ville Kopio <ville.kopio@kodan.fi>
2021-03-20 12:01:25 +0200

    Change data_process to correctly return all labels.

--- src/data_process.py
+++ src/data_process.py
@@ -1,28 +1,22 @@
 import glob
 import os
-import pprint
-import numpy as np
-
-path = 'data/raw/annotations'
-
-image_index_labels = {}
-
-labels = []
-
-for file_path in glob.glob(os.path.join(path, '*.txt')):
-    with open(os.path.join(os.getcwd(), file_path), 'r') as f:
-        label = file_path.split('.')[0].split('/')[-1]
-
-        for row in f:
-            image_index = row.strip()
-            image_index_labels[image_index] = label
 
 
-for index in range(1, 20001):
-    try:
-        labels.append(image_index_labels[str(index)])
-    except:
-        labels.append('other')
+def get_labels():
+    path = 'data/annotations'
+    labels = []
+    label_files = glob.glob(os.path.join(path, '*.txt'))
+    label_files.sort()
 
-with open('data/labels.txt', 'w') as f:
-    f.write('\n'.join(labels) + '\n')
+    for index in range(0, 20000):
+        labels.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    for label_index, file_path in enumerate(label_files):
+        with open(os.path.join(os.getcwd(), file_path), 'r') as f:
+            label = file_path.split('.')[0].split('/')[-1]
+
+            for row in f:
+                image_index = int(row.strip()) - 1
+                labels[image_index][label_index] = 1
+
+    return labels
