f64a0b5c20dbc66ddbbdc634b568ce9f480dc781
Saukkoriipi <saukkoriipi.mikko@gmail.com>
2021-03-31 14:47:03 +0300

    New transforms

--- src/data_set.py
+++ src/data_set.py
@@ -58,14 +58,17 @@ X_train, X_test_val, y_train, y_test_val = data_split(img, lab, test_size=0.2)
 X_test, X_val, y_test, y_val = data_split(X_test_val, y_test_val, test_size=0.5)
 
 transformers_train = [
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.RandomHorizontalFlip(p=0.3),
-    transforms.Normalize(mean=0.5, std=0.5, inplace=True),
+    transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
 ]
 
 transformers = [
+    transforms.Resize(224),
     transforms.ToTensor(),
-    transforms.Normalize(mean=0.5, std=0.5, inplace=True),
+    transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    
 ]
 
 train_set = ImageDataSet(X_train, y_train, transforms.Compose(transformers_train))
